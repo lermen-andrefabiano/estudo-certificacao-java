aee3cbf2bfcdd1c8945cdb64cb94b4c0d4a76c1b
andre.lermen <andre.lermen@MVBRPFBDSK5181.mvpfb.local>
2014-06-02 14:11:44 -0300

    metodo 2

--- String/src/br/com/string/split/Main.java
+++ String/src/br/com/string/split/Main.java
@@ -3,7 +3,9 @@ package br.com.string.split;
 public class Main {
 
 	public static void main(String[] args) {
-		metodoSplit();
+		metodoSplit1();
+		
+		metodoSplit2();
 	}
 
 	/**
@@ -11,7 +13,7 @@ public class Main {
 	 * Metódo split
 	 * 
 	 */
-	static void metodoSplit() {
+	static void metodoSplit1() {
 		double valor = 1.96;
 
 		String str = String.valueOf(valor);
@@ -23,5 +25,17 @@ public class Main {
 		System.out.println(aux[0]);
 		System.out.println(aux[1]);
 
+	}
+	
+	static void metodoSplit2() {
+		
+		String str = "minha:mao:tem5dedo?sim";
+		
+		String vetor[] = str.split("\\:|5|\\?");
+		
+		for(int i = 0; i < vetor.length; i++){
+			System.out.println(vetor[i]);
+		}
+		
 	}
 }
