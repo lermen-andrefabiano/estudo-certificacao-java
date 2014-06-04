bc62d7a5510c04f0a7770cf44579c547fa645bb0
andre.lermen <andre.lermen@MVBRPFBDSK5181.mvpfb.local>
2014-06-04 09:51:59 -0300

    string

---
+++ String/src/br/com/string/ClassString.java
@@ -0,0 +1,17 @@
+package br.com.string;
+
+public class ClassString {
+
+	public static void main(String[] args) {
+
+		// declaração e instanciação (mais prático)
+		String ola = "Alô Mundo Java !";
+		String nome = "Prof. Omero Francisco Bertol.";
+
+		// concatenação (\n = pula linha)
+		String aula = ola + "\nby " + nome;
+		System.out.println(aula);
+
+	}
+
+}
