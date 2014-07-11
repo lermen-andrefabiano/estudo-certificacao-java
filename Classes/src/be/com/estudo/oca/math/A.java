6da40c7abc2fffb633d55a1eeb04e0cca872263b
andre.lermen <andre.lermen@MVBRPFBDSK5181.mvpfb.local>
2014-07-11 17:16:54 -0300

    Math

---
+++ Classes/src/be/com/estudo/oca/math/A.java
@@ -0,0 +1,19 @@
+package be.com.estudo.oca.math;
+
+public class A {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.floor(-4.7));
+		
+		System.out.println(Math.round(-4.7));
+		
+		System.out.println(Math.ceil(-4.7));
+		
+		//System.out.println(Math.min(-4.7));
+		
+		System.out.println(Math.round(-4.1));
+		
+		System.out.println(Math.round(-4.5));
+	}
+}
