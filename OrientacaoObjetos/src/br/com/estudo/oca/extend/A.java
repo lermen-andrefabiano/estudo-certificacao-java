40100813c19496aadb943f85d3d0a6678f25a899
andre.lermen <andre.lermen@MVBRPFBDSK5181.mvpfb.local>
2014-07-11 17:06:10 -0300

    interfaces

---
+++ OrientacaoObjetos/src/br/com/estudo/oca/extend/A.java
@@ -0,0 +1,13 @@
+package br.com.estudo.oca.extend;
+
+public class A {
+
+	public A() {
+		System.out.println("A1");
+	}
+	
+	public void um(){
+		System.out.println("um");
+	}
+	
+}
---
+++ OrientacaoObjetos/src/br/com/estudo/oca/extend/B.java
@@ -0,0 +1,12 @@
+package br.com.estudo.oca.extend;
+
+public class B extends A{
+
+	public static void main(String[] args) {
+		System.out.println("B");
+		
+		B b = new B();	
+		
+	}
+	
+}
