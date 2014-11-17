38bd4ff2e71c764329f3bd922325556a40354414
andre.lermen <lermen.andrefabiano@gmail.com>
2014-11-17 14:47:07 -0200

    enums

---
+++ Classes/src/be/com/estudo/oca/enuns/CoffeType.java
@@ -0,0 +1,11 @@
+package be.com.estudo.oca.enuns;
+
+public enum CoffeType {
+
+	CURTO,
+
+	LONGO,
+
+	CAPUCINO
+
+}
---
+++ Classes/src/be/com/estudo/oca/enuns/CoffeTypeMain.java
@@ -0,0 +1,15 @@
+package be.com.estudo.oca.enuns;
+
+import java.lang.reflect.Field;
+
+public class CoffeTypeMain {
+	
+	public static void main(String[] args) {
+		Field [] f = CoffeType.class.getDeclaredFields();
+		
+		for(int i = 0; i < f.length; i++){
+			System.out.println(f[i]);			
+		}
+	}
+
+}
