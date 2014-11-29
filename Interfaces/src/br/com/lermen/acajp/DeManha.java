00f85415e71a7011f5df7294b76c98891b2f7e5e
Andre <Andre@Andre-PC>
2014-11-29 11:30:17 -0200

    interface, para fechar o capitulo um com chaver de ouro
    vamo q vamo

---
+++ Interfaces/src/br/com/lermen/acajp/DeManha.java
@@ -0,0 +1,7 @@
+package br.com.lermen.acajp;
+
+public interface DeManha {
+	
+	void primeiroCapituloPronto();
+
+}
---
+++ Interfaces/src/br/com/lermen/acajp/ShowDeBola.java
@@ -0,0 +1,11 @@
+package br.com.lermen.acajp;
+
+public abstract class ShowDeBola implements Sabado {
+
+	final void go() {
+		System.out.println("Deus me ajude");
+	}
+	
+	public abstract void livro();
+
+}
