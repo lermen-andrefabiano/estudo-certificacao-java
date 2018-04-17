fa3b862fefcf4e02a55ff96b405fc556d31ef4e8
Andre Fabiano Lermen <lermen.andrefabiano@gmail.com>
2018-04-17 15:06:46 -0300

    Herança

---
+++ Heranca/src/br/com/lermen/ocajp/heranca/Mammal.java
@@ -0,0 +1,9 @@
+package br.com.lermen.ocajp.heranca;
+
+class Mammal {
+
+	public Mammal() {
+		System.out.println("Mammal");
+	}
+	
+}
---
+++ Heranca/src/br/com/lermen/ocajp/heranca/Platypus.java
@@ -0,0 +1,13 @@
+package br.com.lermen.ocajp.heranca;
+
+public class Platypus extends Mammal{
+	
+	public Platypus() {
+		System.out.println("Platypus");
+	}
+	
+	public static void main(String[] args) {
+		new Platypus();
+	}
+
+}
